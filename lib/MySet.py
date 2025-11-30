45e867add9bd461c87377b8ebcd0103198ebe8f4
Ahman04 <abokey2020@gmail.com>
2025-11-30 11:53:26 +0300

    Add MySet implementation passing all tests

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,24 @@
+# MySet.py
+
 class MySet:
 
-    pass
+    def __init__(self, elements=None):
+        self.dictionary = {}
+        if elements:
+            for el in elements:
+                self.dictionary[el] = True
+
+    def add(self, element):
+        """Add element to the set."""
+        self.dictionary[element] = True
+
+    def delete(self, element):
+        """Delete element from the set."""
+        if element in self.dictionary:
+            del self.dictionary[element]
+
+    def has(self, element):
+        """Return True if element in set."""
+        return element in self.dictionary
+
+   
